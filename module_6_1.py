2816d315ef27ab406cd47fd188fc74709b3d7375
PAvel <95528324+Pavel-L-40@users.noreply.github.com>
2024-10-04 09:04:32 +0300

    переопределение атрибутов классов и экземппляров/оптимизация
    def eat должен быть только в родительском классе...

--- module_6_1.py
+++ module_6_1.py
@@ -1,14 +1,10 @@
-#=======================Animal=======================
+#=======================Animal========================
 class Animal:
-
-
-    def __init__(self, name_animal, alive = True, fed = False):
+    alive = True  # живой
+    fed = False  # сытый
+    def __init__(self, name_animal):
         self.name = name_animal
-        self.alive = alive      # живой
-        self.fed = fed          # сытый
 
-#--------------''' наследники Animal'''
-class Mammal(Animal):
     def eat(self, food):
         if food.edible:
             print(f'{self.name} ест {food.name}')
@@ -17,32 +13,33 @@ class Mammal(Animal):
             print(f'{self.name} не стал есть {food.name}')
             self.alive = False
 
-class Predator(Animal):
-    def eat(self, food):
-        if food.edible:
-            print(f'{self.name} съел {food.name}')
-            self.fed = True
-        else:
-            print(f'{self.name} не стал есть {food.name}')
-            self.alive = False
 
+#--------------''' наследники Animal'''-------------
+class Mammal(Animal):
+    pass
+class Predator(Animal):
+    pass
+#===================================================
 
 
 #=======================Plant=======================
 class Plant:
-     # съедобность
-    def __init__(self, name_plant,edible = False):
+    edible = False # съедобность
+    def __init__(self, name_plant):
         self.name = name_plant
-        self.edible = edible
-#--------------''' наследники Plant'''
+
+#--------------''' наследники Plant'''-------------
 class Flower(Plant):
     pass # параметры наследования не изменяются
 
 
 class Fruit(Plant):
-    def __init__(self, name, edible = True ):
-        self.name = name
-        self.edible = True
+    def __init__(self, name_plant, edible = True):
+        super().__init__(name_plant)
+        self.edible = edible
+
+# ---End code---
+
 
 # flower1 = Flower('Хризантема')
 # fruit1 = Fruit('apple')
@@ -55,6 +52,7 @@ a2 = Mammal('Хатико')
 p1 = Flower('Цветик семицветик')
 p2 = Fruit('Заводной апельсин')
 
+
 print(a1.name)
 print(p1.name)
 
@@ -64,3 +62,6 @@ a1.eat(p1)
 a2.eat(p2)
 print(a1.alive)
 print(a2.fed)
+
+# a3 = Predator('King_Kong', fed = True)
+# print(a3.fed)
